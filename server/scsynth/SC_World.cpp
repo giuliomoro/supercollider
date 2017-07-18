3cdc9d0d838e69c4cff406f768e60346b74662ce
Marije Baalman <nescivi@gmail.com>
2017-07-19 00:39:53 +0200

    BELA: scsynth: fix variable names for belaSettings

--- server/scsynth/SC_World.cpp
+++ server/scsynth/SC_World.cpp
@@ -452,7 +452,7 @@ World* World_New(WorldOptions *inOptions)
 			world->hw->mPassword[0] = 0;
 		}
 #ifdef BELA
-        world->mBelaInitSettings = &(inOptions->mBelaInitSettings); // just a pointer
+        world->mBelaSettings = &(inOptions->mBelaSettings); // just a pointer
 #endif
 
 #ifdef __APPLE__
