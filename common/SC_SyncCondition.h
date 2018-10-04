5fe3ba3b17f6759263366c1461c22c093f6caa71
Giulio Moro <giuliomoro@yahoo.it>
2018-10-04 01:09:56 +0000

    PoC for xenomai-friendly condition variable. Outstanding issues: the waiting thread has to be turned into a Xenomai thread, mode switches may happen there. Maybe a Xenomai pipe (RTIPC) would be better? Towards BelaPlatform/supercollider#62

--- common/SC_SyncCondition.h
+++ common/SC_SyncCondition.h
@@ -20,26 +20,62 @@
 
 #pragma once
 
+#ifdef BELA
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+extern int gXenomaiInited;
+#include <xenomai/init.h>
+#include <sched.h>
+#else /* BELA */
 #include "SC_Lock.h"
+#endif /* BELA */
 
 class SC_SyncCondition
 {
 public:
 	SC_SyncCondition()
 		: read(0), write(0)
-	{}
+	{
+#ifdef BELA
+		if(!gXenomaiInited)
+		{
+			int argc = 0;
+			char *const *argv;
+			xenomai_init(&argc, &argv);
+			gXenomaiInited = 1;
+		}
+		if(int ret = __wrap_pthread_cond_init(&available, NULL))
+		{
+			fprintf(stderr, "Unable to initialize condition variable: %d %s\n", ret, strerror(ret));
+		}
+		if(int ret = __wrap_pthread_mutex_init(&mutex, NULL))
+		{
+			fprintf(stderr, "Unable to initialize mutex %d %s\n", ret, strerror(ret));
+		}
+		int ret = __wrap_pthread_mutex_lock(&mutex);
+		if(ret)
+			printf("ctor mutex_lock ret: %d %s\n", ret, strerror(ret));
+		ret = __wrap_pthread_mutex_unlock(&mutex);
+		if(ret)
+			printf("ctor mutex_unlock ret: %d %s\n", ret, strerror(ret));
+#endif /* BELA */
+	}
 
 	~SC_SyncCondition()
 	{}
 
 	void WaitEach()
 	{
+#ifdef BELA
+#else /* BELA */
 		// waits if it has caught up.
 		// not very friendly, may be trying in vain to keep up.
 		unique_lock<SC_Lock> lock(mutex);
 		while (read == write)
 			available.wait(lock);
 		++read;
+#endif /* BELA */
 	}
 
 	void WaitOnce()
@@ -47,11 +83,14 @@ public:
 		// waits if not signaled since last time.
 		// if only a little late then can still go.
 
+#ifdef BELA
+#else /* BELA */
 		unique_lock<SC_Lock> lock(mutex);
 		int writeSnapshot  = write;
 		while (read == writeSnapshot)
 			available.wait(lock);
 		read = writeSnapshot;
+#endif /* BELA */
 	}
 
 	void WaitNext()
@@ -59,23 +98,74 @@ public:
 		// will wait for the next signal after the read = write statement
 		// this is the friendliest to other tasks, because if it is
 		// late upon entry, then it has to lose a turn.
+#ifdef BELA
+		//nothing
+#else /* BELA */
 		unique_lock<SC_Lock> lock(mutex);
+#endif /* BELA */
 		read = write;
 		while (read == write)
+		{
+#ifdef BELA
+			int ret = __wrap_pthread_mutex_lock(&mutex);
+			if(ret)
+			{
+				printf("mutex_lock ret: %d %s\n", ret, strerror(ret));
+				struct sched_param_ex param;
+				param.sched_priority = 1;
+
+				ret = sched_setscheduler_ex(0, SCHED_FIFO, &param);
+				printf("setscheduler: %d\n", ret);
+				return;
+			}
+
+			ret = __wrap_pthread_cond_wait(&available, &mutex);
+			if(ret)
+			{
+				printf("pthread_cond_wait ret: %d %s\n", ret, strerror(ret));
+				usleep(100000);
+				break;
+			}
+			ret = __wrap_pthread_mutex_unlock(&mutex);
+			if(ret)
+				printf("mutex_unlock ret: %d %s\n", ret, strerror(ret));
+#else /* BELA */
 			available.wait(lock);
+#endif /* BELA */
+		}
 	}
 
 	void Signal()
 	{
 		++write;
+#ifdef BELA
+	int ret;
+	if(ret = __wrap_pthread_mutex_trylock(&mutex))
+	{
+	// If we cannot get the lock, then the other thread is probably still running.
+	} else {
+		ret = __wrap_pthread_cond_signal(&available);
+		if(ret)
+			printf("signal: cond_signal ret: %d %s\n", ret, strerror(ret));
+		ret = __wrap_pthread_mutex_unlock(&mutex);
+		if(ret)
+			printf("signal: mutex_unlock (rt) ret: %d %s\n", ret, strerror(ret));
+	}
+#else /* BELA */
 		available.notify_one();
+#endif /* BELA */
 	}
 
 private:
 	// the mutex is only for pthread_cond_wait, which requires it.
 	// since there is only supposed to be one signaller and one waiter
 	// there is nothing to mutually exclude.
+#ifdef BELA
+	pthread_cond_t available;
+	pthread_mutex_t mutex;
+#else /* BELA */
 	condition_variable_any available;
 	SC_Lock mutex;
+#endif /* BELA */
 	int read, write;
 };
